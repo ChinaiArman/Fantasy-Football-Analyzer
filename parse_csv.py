0eb5ed0e34a2c210440f1a63242d3ef651202b32
ChinaiArman <chinaiarman@gmail.com>
2022-12-14 20:47:27 -0800

    Change File Location

--- parse_csv.py
+++ parse_csv.py
@@ -5,16 +5,15 @@ A python script to parse multiple CSV files and create a single CSV file.
 """
 
 
-# import pandas as pd
-# import glob
-# import os
-# import numpy as np
-
-# # RB_CSVS = ['data_rb_finishes.csv', 'data_rb_sos.csv']
+import pandas as pd
+import glob
 import os
+import numpy as np
+
+
 RB_CSVS = [file for file in os.listdir("c:/Users/arman/OneDrive/Documents/GitHub/Fantasy-Football-Analyzer") if file.startswith('data_rb')]
 TEAM_CSVS = [file for file in os.listdir("c:/Users/arman/OneDrive/Documents/GitHub/Fantasy-Football-Analyzer") if file.startswith('data_team')]
-
+print(os.getcwd())
 
 
 def create_combined_csv():
