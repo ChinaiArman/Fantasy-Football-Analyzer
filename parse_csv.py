02bc59619c5b2db5907e49bc92fd8283a9bdfbf1
ChinaiArman <chinaiarman@gmail.com>
2022-12-14 21:40:39 -0800

    Make create_dataframe function

--- parse_csv.py
+++ parse_csv.py
@@ -11,21 +11,21 @@ import os
 import numpy as np
 
 
-RB_CSVS =[file for file in glob.glob('data_rb*.csv')]
-TEAM_CSVS =[file for file in glob.glob('data_team*.csv')]
+RB_CSVS = [file for file in glob.glob('data_rb*.csv') if 'stats' not in file]
+TEAM_CSVS = [file for file in glob.glob('data_team*.csv')]
+IGNORED_COLUMNS = ['Ovr', 'Name', 'Team']
 
 
-def create_combined_csv():
-    pass
-    # print(RB_CSVS)
-    # df = pd.read_csv(RB_CSVS[0])
-    # print('hi')
-    # print(df.head())
-    # print('hey')
+def create_dataframe(file):
+    df = pd.read_csv(file)
+    for column in IGNORED_COLUMNS:
+        df = df.loc[:, df.columns != column]
+    return df
 
 
 def main():
-    create_combined_csv()
+    dataframes = [create_dataframe(file) for file in RB_CSVS]
+    
 
 
 if __name__ == "__main__":
