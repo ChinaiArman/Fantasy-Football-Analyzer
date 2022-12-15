17c33a5039dfbd49347fb3c393762a5354c8b7b5
ChinaiArman <chinaiarman@gmail.com>
2022-12-14 20:42:45 -0800

    Create CSV Constants

--- parse_csv.py
+++ parse_csv.py
@@ -5,16 +5,29 @@ A python script to parse multiple CSV files and create a single CSV file.
 """
 
 
-import pandas as pd
-import glob
+# import pandas as pd
+# import glob
+# import os
+# import numpy as np
+
+# # RB_CSVS = ['data_rb_finishes.csv', 'data_rb_sos.csv']
 import os
-import numpy as np
+RB_CSVS = [file for file in os.listdir("c:/Users/arman/OneDrive/Documents/GitHub/Fantasy-Football-Analyzer") if file.startswith('data_rb')]
+TEAM_CSVS = [file for file in os.listdir("c:/Users/arman/OneDrive/Documents/GitHub/Fantasy-Football-Analyzer") if file.startswith('data_team')]
 
 
+
+def create_combined_csv():
+    pass
+    # print(RB_CSVS)
+    # df = pd.read_csv(RB_CSVS[0])
+    # print('hi')
+    # print(df.head())
+    # print('hey')
 
 
 def main():
-    pass
+    create_combined_csv()
 
 
 if __name__ == "__main__":
