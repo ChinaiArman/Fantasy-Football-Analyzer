537171d8ffe12aa6ea4773dcd158f285dcfbe9d0
ChinaiArman <chinaiarman@gmail.com>
2022-12-14 20:03:59 -0800

    imports and if __name__ statement

--- parse_csv.py
+++ parse_csv.py
@@ -3,3 +3,19 @@ Arman Chinai
 
 A python script to parse multiple CSV files and create a single CSV file.
 """
+
+
+import pandas as pd
+import glob
+import os
+import numpy as np
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
