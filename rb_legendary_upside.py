3449193dfe392f6c73c0b8e9feb7ae30c1744163
ChinaiArman <chinaiarman@gmail.com>
2022-12-17 14:47:09 -0800

    Add comments to legendary_upside

--- rb_legendary_upside.py
+++ rb_legendary_upside.py
@@ -44,12 +44,21 @@ def main() -> None:
     """
     Execute the program.
     """
+    # Read the relevant columns from the RB Data and store as a Pandas DataFrame.
     legendary_runningback_candidates = pd.read_csv(COMPILED_RB_DATA, usecols = LEGENDARY_RB_REL_COLUMNS, low_memory = True)
+
+    # Add extra datapoints necessary for Legendary RB Calculations.
     legendary_runningback_candidates = md.add_extra_datapoints(legendary_runningback_candidates, OL_RANK, 0, 1, 'olRank', base_index = 1)
     legendary_runningback_candidates = md.add_extra_datapoints(legendary_runningback_candidates, TEAM_TARGETS, 0, 7, 'teamTargets', base_index = 1)
+
+    # Remove RBs that do not meet the criteria for Legendary Upside.
     legendary_runningbacks = remove_non_legendary_rbs(legendary_runningback_candidates)
+
+    # Fix Indexes.
     legendary_runningbacks.reset_index(inplace=True)
     legendary_runningbacks.drop('index', axis=1, inplace=True)
+
+    # Push to CSV file.
     if not os.path.exists(CALCULATIONS_FOLDER):
         final_directory = os.path.join(os.getcwd(), CALCULATIONS_FOLDER)
         os.makedirs(final_directory)
