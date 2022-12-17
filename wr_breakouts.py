1620ed4c2e962e6e12f3957103e3cacf71bc0735
ChinaiArman <chinaiarman@gmail.com>
2022-12-16 22:33:15 -0800

    Create wr_breakouts.py

---
+++ wr_breakouts.py
@@ -0,0 +1,23 @@
+"""
+A python module with functions designed to identify breakout WRs.
+"""
+
+
+# Imports
+import pandas as pd
+import os
+
+import merge_dataframes as md
+
+
+# Constants
+YEAR = 2022
+RELEVANT_COLUMNS = []
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
