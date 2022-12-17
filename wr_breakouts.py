4530a7a7d89544e7bdfd782c831a7aa472e25605
ChinaiArman <chinaiarman@gmail.com>
2022-12-16 22:37:29 -0800

    Create necessary function templates

--- wr_breakouts.py
+++ wr_breakouts.py
@@ -15,6 +15,25 @@ YEAR = 2022
 RELEVANT_COLUMNS = []
 
 
+def create_wr_csv() -> None:
+    """
+    Create a containing all the WR data for WRs with an ADP.
+
+    :return: None.
+    """
+    pass
+
+
+def remove_non_breakout_wr(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Remove WRs that do not have breakout potential from a DataFrame.
+    
+    :param dataframe: A dataframe containing WR player data.
+    :return: A dataframe, containing the WRs that have breakout potential.
+    """
+    pass
+
+
 def main() -> None:
     pass
 
