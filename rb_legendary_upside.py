85a5ee937d0570fd0fb1b140b11e824a3304b744
ChinaiArman <chinaiarman@gmail.com>
2022-12-16 19:51:14 -0800

    Update rb_legendary_upside.py

--- rb_legendary_upside.py
+++ rb_legendary_upside.py
@@ -28,16 +28,17 @@ def create_rb_csv() -> None:
 
     :return: None.
     """
-    primary_dataframe = pd.read_csv(MAIN_RB_CSV).sort_values('player')
-    dataframes = [md.create_dataframe(file, 'Name', 3) for file in RB_CSVS]
-    primary_dataframe = md.create_combined_dataframe(primary_dataframe, dataframes)
-    primary_dataframe = md.add_extra_datapoints(primary_dataframe, PLAYER_ADPS, 1, 11, 'ADP')
-    primary_dataframe = md.add_extra_datapoints(primary_dataframe, PLAYER_AGE, 1, 4, 'age')
-    primary_dataframe = primary_dataframe.sort_values('ADP')
-    primary_dataframe = primary_dataframe.dropna(subset=['ADP'])
-    primary_dataframe.reset_index(inplace=True)
-    primary_dataframe.drop('index', axis=1, inplace=True)
-    primary_dataframe.to_csv(f'./{YEAR - 1}_data/compiled_rb_data.csv')
+    if not os.path.exists(f'./{YEAR - 1}_data/compiled_rb_data.csv'):
+        primary_dataframe = pd.read_csv(MAIN_RB_CSV).sort_values('player')
+        dataframes = [md.create_dataframe(file, 'Name', 3) for file in RB_CSVS]
+        primary_dataframe = md.create_combined_dataframe(primary_dataframe, dataframes)
+        primary_dataframe = md.add_extra_datapoints(primary_dataframe, PLAYER_ADPS, 1, 11, 'ADP')
+        primary_dataframe = md.add_extra_datapoints(primary_dataframe, PLAYER_AGE, 1, 4, 'age')
+        primary_dataframe = primary_dataframe.sort_values('ADP')
+        primary_dataframe = primary_dataframe.dropna(subset=['ADP'])
+        primary_dataframe.reset_index(inplace=True)
+        primary_dataframe.drop('index', axis=1, inplace=True)
+        primary_dataframe.to_csv(f'./{YEAR - 1}_data/compiled_rb_data.csv')
 
 
 def remove_non_legendary_runningbacks(dataframe: pd.DataFrame) -> pd.DataFrame:
@@ -65,8 +66,7 @@ def main() -> None:
     """
     Execute the program.
     """
-    if not os.path.exists(f'./{YEAR - 1}_data/compiled_rb_data.csv'):
-        create_rb_csv()
+    create_rb_csv()
     legendary_runningback_candidates = pd.read_csv(f'./{YEAR - 1}_data/compiled_rb_data.csv', usecols = RELEVANT_COLUMNS, low_memory = True)
     legendary_runningback_candidates = md.add_extra_datapoints(legendary_runningback_candidates, OL_RANK, 0, 1, 'olRank', base_index = 1)
     legendary_runningback_candidates = md.add_extra_datapoints(legendary_runningback_candidates, TEAM_TARGETS, 0, 7, 'teamTargets', base_index = 1)
