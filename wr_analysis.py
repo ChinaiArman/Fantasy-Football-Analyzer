42896e56211bc9e8f55a9c30ef2bfad3f9280644
ChinaiArman <chinaiarman@gmail.com>
2022-12-20 17:13:51 -0800

    Create breakout receiver analysis

--- wr_analysis.py
+++ wr_analysis.py
@@ -15,9 +15,9 @@ YEAR = 2022
 CALCULATIONS_FOLDER = f'./{YEAR}_calculations'
 COMPILED_WR_DATA = f'./{YEAR - 1}_data/compiled_wr_data.csv'
 BREAKOUT_WR_FILE = f'./{YEAR}_calculations/breakout_receivers.csv'
-BREAKOUT_WR_REL_COLUMNS = []
-# OL_RANK = f'./{YEAR - 1}_data/data_team_olrank.csv'
-# TEAM_TARGETS = f'./{YEAR - 1}_data/data_team_trgt%.csv'
+BREAKOUT_WR_REL_COLUMNS = ['player', 'team', 'games', 'recTarg', 'ADP', 'age']
+REC_GRADE = f'./{YEAR - 1}_data/data_player_receivinggrade.csv'
+TEAM_TARGETS = f'./{YEAR - 1}_data/data_team_trgt%.csv'
 
 
 def remove_non_breakout_wr(dataframe: pd.DataFrame) -> pd.DataFrame:
@@ -27,11 +27,37 @@ def remove_non_breakout_wr(dataframe: pd.DataFrame) -> pd.DataFrame:
     :param dataframe: A dataframe containing WR player data.
     :return: A dataframe, containing the WRs that have breakout potential.
     """
-    pass
+    dataframe['trgt%'] = (dataframe['recTarg'] / ((dataframe['teamTargets'] / 17) * dataframe['games'])) * 100
+    dataframe = dataframe[(dataframe['ADP'] >= 30) & (dataframe['ADP'] <= 100)]
+    dataframe = dataframe[dataframe['trgt%'] >= 20]
+    dataframe = dataframe[dataframe['recGrade'] >= 78]
+    dataframe = dataframe[dataframe['age'] <= 25]
+    removable_elements = [element for element in dataframe.columns if element not in ['player', 'team', 'age', 'ADP']]
+    dataframe = dataframe.drop((element for element in removable_elements), axis=1)
+    return dataframe
 
 
 def main() -> None:
-    pass
+    # BREAKOUT RECEIVERS
+    # Read the relevant columns from the WR Data and store as a Pandas DataFrame.
+    breakout_receiver_candidates = pd.read_csv(COMPILED_WR_DATA, usecols = BREAKOUT_WR_REL_COLUMNS, low_memory = True)
+
+    # Add extra datapoints necessary for Breakout Receivers Calculations.
+    breakout_receiver_candidates = md.add_extra_datapoints(breakout_receiver_candidates, TEAM_TARGETS, 0, 7, 'teamTargets', base_index = 1)
+    breakout_receiver_candidates = md.add_extra_datapoints(breakout_receiver_candidates, REC_GRADE, 0, 21, 'recGrade', base_index = 0)
+
+    # Remove RBs that do not meet the criteria for Breakout Potential.
+    breakout_receivers = remove_non_breakout_wr(breakout_receiver_candidates)
+
+    # Fix Indexes.
+    breakout_receivers.reset_index(inplace=True)
+    breakout_receivers.drop('index', axis=1, inplace=True)
+
+    # Push to CSV file.
+    if not os.path.exists(CALCULATIONS_FOLDER):
+        final_directory = os.path.join(os.getcwd(), CALCULATIONS_FOLDER)
+        os.makedirs(final_directory)
+    breakout_receivers.to_csv(BREAKOUT_WR_FILE)
 
 
 if __name__ == '__main__':
