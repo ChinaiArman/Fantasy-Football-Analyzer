de4a224cd6a9bed01885614b2170d17a79180b0f
ChinaiArman <chinaiarman@gmail.com>
2022-12-23 21:46:00 -0800

    Create __name__ statement

--- qb_analysis.py
+++ qb_analysis.py
@@ -16,3 +16,11 @@ CALCULATIONS_FOLDER = f'./{YEAR}_calculations'
 COMPILED_RB_DATA = f'./{YEAR - 1}_data/compiled_rb_data.csv'
 MUST_DRAFT_QBS_FILE = f'./{YEAR}_calculations/must_draft_quarterbacks.csv'
 MUST_DRAFT_QB_REL_COLUMNS = []
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
