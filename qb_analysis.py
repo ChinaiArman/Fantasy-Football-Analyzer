9fd3d4933a93621cdce57ee4e31d96a8addacd3d
ChinaiArman <chinaiarman@gmail.com>
2022-12-23 21:26:50 -0800

    Create constants and imports

---
+++ qb_analysis.py
@@ -0,0 +1,18 @@
+"""
+A python module with functions designed to identify RBs with legendary upside.
+"""
+
+
+# Imports
+import pandas as pd
+import os
+
+import merge_dataframes as md
+
+
+# Constants
+YEAR = 2022
+CALCULATIONS_FOLDER = f'./{YEAR}_calculations'
+COMPILED_RB_DATA = f'./{YEAR - 1}_data/compiled_rb_data.csv'
+MUST_DRAFT_QBS_FILE = f'./{YEAR}_calculations/must_draft_quarterbacks.csv'
+MUST_DRAFT_QB_REL_COLUMNS = []
